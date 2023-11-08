d442a80f96df7547963f07b1aa1ef0667c49e769
Mr. Mendez <mrmendez@amvestor.com>
2023-11-08 12:37:07 -0500

    markup and styles done, no logic yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TodolistComponent } from "./todolist/todolist.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [CommonModule, RouterOutlet, TodolistComponent]
 })
 export class AppComponent {
   title = 'todo';
